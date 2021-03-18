88e60f3f4ee8ec9c75061fdb55be0d92df33bbe4
randy554 <randy554@hotmail.com>
2021-03-19 00:19:23 +0100

    html forms setup

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get("/course", (req, res) => {
   res.render("course");
 });
 
+app.get("/assesment", (req, res) => {
+  res.render("assesment");
+});
+
+app.get("/successpage", (req, res) => {
+  res.render("successpage");
+});
+
 app.listen(process.env.PORT || 4000, () =>
   console.log(`Open page @ http://localhost:${process.env.PORT}`)
 );
